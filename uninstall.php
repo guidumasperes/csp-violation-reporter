6fd23ca3994ec9fbacd24b8acc2f524de17acaf2
guidumasperes <guidumasperes@gmail.com>
2026-05-25 12:09:20 -0300

    Fix Plugin Check findings

--- uninstall.php
+++ uninstall.php
@@ -11,6 +11,6 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 
-$table_name = $wpdb->prefix . 'cspvr_reports';
-$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i', $table_name ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.DirectDatabaseQuery.SchemaChange -- Drops the plugin's custom table only during uninstall.
+$cspvr_table_name = $wpdb->prefix . 'cspvr_reports';
+$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i', $cspvr_table_name ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.DirectDatabaseQuery.SchemaChange -- Drops the plugin's custom table only during uninstall.
 delete_option( 'cspvr_table_version' );
